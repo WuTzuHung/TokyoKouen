9e5786b8b617bb030eb76410907211eb72d240d2
WuTzuHung <not5508085@gmail>
2024-04-16 20:53:02 +0800

    0416_0852

--- src/stores/abc.js
+++ src/stores/abc.js
@@ -191,7 +191,7 @@ export default defineStore("data", {
             })
         },
         getWenhua() {
-            fetch("bunnka/bunnka.json")
+            fetch("/bunnka/bunnka.json")
                 .then(response => response.json())
                 .then(data => {
                     this.objb = data
@@ -199,7 +199,7 @@ export default defineStore("data", {
                 })
         },
         getSport() {
-            fetch("src/components/API.JSON")
+            fetch("/src/components/API.JSON")
                 .then(response => response.json())
                 .then(data => {
                     this.objs = data
